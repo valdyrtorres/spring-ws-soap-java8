e4a5c2127b423bc9a1d16f4562c775c3b72deaf5
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2022-03-04 10:17:28 -0300

    first commit

---
+++ src/main/java/com/codenotfound/SpringWsHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.codenotfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsHelloWorldApplication.class, args);
+	}
+
+}
